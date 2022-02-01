066aee094453ec69bf490a96435fb33c97b0a721
Parinay Chauhan <parinayc20@iitk.ac.in>
2022-02-01 12:41:24 +0530

    First commit

---
+++ backend/src/main/java/com/exec/demo/Greetings.java
@@ -0,0 +1,15 @@
+package com.exec.demo;
+
+public class Greetings {
+    
+    private final String content;
+
+    public Greetings(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    
+}
