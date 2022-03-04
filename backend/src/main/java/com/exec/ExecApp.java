042f8d21b09a67e91d1d2d87025cd057fbe5d74f
Parinay Chauhan <parinayc20@iitk.ac.in>
2022-03-04 15:19:54 +0530

    Login added

--- backend/src/main/java/com/exec/ExecApp.java
+++ backend/src/main/java/com/exec/ExecApp.java
@@ -1,10 +1,11 @@
 package com.exec;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ExecApp {
     public static void main(String[] args) {
         SpringApplication.run(ExecApp.class, args);
--- backend/src/main/java/com/exec/controller/GBMController.java
+++ backend/src/main/java/com/exec/controller/GBMController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RequestMapping("/api/GBM")
 @RestController
@@ -41,11 +43,65 @@ public class GBMController {
     public ResponseEntity<Object> signup(@RequestBody Map<String, String> body) {
         try
         {
+            GBM gbm;
+            Map<String, String> response = new HashMap<String, String>();
+
+            try{
+                gbm = gbmservice.getGBMByRoll(body.get("roll_no"));
+            }
+            catch(Exception E){
+                response.put("message", "No student with this roll no.");
+                return new ResponseEntity<Object>(response, HttpStatus.UNAUTHORIZED);
+            }
+
+            if(gbm.is_activated == true)
+            {
+                response.put("message", "Student already signed up");
+                return new ResponseEntity<Object>(response, HttpStatus.UNAUTHORIZED);
+            }
+
             String otp = utils.otpGenerator();
             gbmservice.setOtp(body.get("roll_no"), otp);
-            GBM gbm = gbmservice.getGBMByRoll(body.get("roll_no"));
             emailSender.sendOTPMessage(gbm.email, gbm.name, otp);
-            return ResponseEntity.status(HttpStatus.CREATED).build();
+            return ResponseEntity.status(HttpStatus.OK).build();
+        }
+        catch(Exception E){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @PostMapping("/changePassword")
+    public ResponseEntity<Object> changePassword(@RequestBody Map<String, String> body) {
+
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        try{
+            GBM gbm = gbmservice.getGBMByRoll(body.get("roll_no"));
+            Map<String,String> response = new HashMap<>();
+
+            if(! (gbm.otp).equals(body.get("otp"))){
+                response.put("message", "Invalid OTP");
+                return new ResponseEntity<Object>(response, HttpStatus.UNAUTHORIZED);
+            }
+            
+            String password = passwordEncoder.encode(body.get("password"));
+            gbmservice.activateGBM(body.get("roll_no"), password);
+            return ResponseEntity.status(HttpStatus.OK).build();
+        }
+        catch(Exception E){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody Map<String, String> body) {
+        try{
+            GBM gbm = gbmservice.getGBMByRoll(body.get("roll_no"));
+            PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            if(! passwordEncoder.matches(body.get("password"), gbm.password)){
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+            return ResponseEntity.status(HttpStatus.OK).build();
         }
         catch(Exception E){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
